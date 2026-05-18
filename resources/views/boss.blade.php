c86e6a63480e67ac9f92f927abeef470fd48f73a
Your Name <skolnieks@vtdt.edu.lv>
2026-05-18 11:01:35 +0300

    push

--- resources/views/boss.blade.php
+++ resources/views/boss.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
+    <link rel="stylesheet" href="boss.css">
 </head>
 <body>
-    <h1>Sveiks, Laravel!</h1>
+    <h1>BOSS</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/why', function () {
+    return view('why');
+});
+
+
+Route::get('/boss', function () {
+    return view('boss');
+});
